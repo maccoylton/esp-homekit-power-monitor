f6602e40bd2b09fed44034ec2ddad54ba8dcd8f6
David B Brown <dave@miniserver.maccoylton.com>
2022-04-01 19:47:22 +0100

    wip in progress

--- src/main.c
+++ src/main.c
@@ -43,6 +43,7 @@
 #include <udplogger.h>
 #include <custom_characteristics.h>
 #include <shared_functions.h>
+#include <esp-homekit-eve-history.h>
 
 // add this section to make your device OTA capable
 // create the extra characteristic &ota_trigger, at the end of the primary service (before the NULL)
@@ -52,7 +53,9 @@
 #include <ota-api.h>
 
 #define SAVE_DELAY 2000
-#define POWER_MONITOR_POLL_PERIOD 3000
+#define POWER_MONITOR_POLL_PERIOD 3000 // 3 seconds * 1000
+#define EVE_HISTORY_LOG_PERIOD 6000000 // 10 minutes * 1000
+#define EVE_HISTORY_LOG_LOOPS 200 // EVE_HISTORY_LOG_PERIOD / POWER_MONITOR_POLL_PERIOD
 
 
 homekit_characteristic_t wifi_check_interval   = HOMEKIT_CHARACTERISTIC_(CUSTOM_WIFI_CHECK_INTERVAL, 10, .setter=wifi_check_interval_set);
@@ -75,6 +78,13 @@ homekit_characteristic_t amps = HOMEKIT_CHARACTERISTIC_(CUSTOM_AMPS, 0);
 homekit_characteristic_t watts = HOMEKIT_CHARACTERISTIC_(CUSTOM_WATTS, 0);
 homekit_characteristic_t switch_on = HOMEKIT_CHARACTERISTIC_(ON, false);
 
+homekit_characteristic_t EVE_history_status_S2R1_116 = HOMEKIT_CHARACTERISTIC_(CUSTOM_EVE_HISTORY_S2R1, 0, 0, .getter=getter_EVE_history_status_S2R1_116 );
+homekit_characteristic_t EVE_history_entries_S2R2_117 = HOMEKIT_CHARACTERISTIC_(CUSTOM_EVE_HISTORY_S2R2, 0, 0, .getter=getter_EVE_history_entries_S2R2_117 );
+homekit_characteristic_t EVE_history_request_S2W1_11C = HOMEKIT_CHARACTERISTIC_(CUSTOM_EVE_HISTORY_S2W1, .setter=setter_EVE_history_request_S2W1_11C);
+homekit_characteristic_t EVE_set_time_S2W2_121 = HOMEKIT_CHARACTERISTIC_(CUSTOM_EVE_HISTORY_S2W2, .setter=setter_EVE_set_time_S2W2_121);
+
+
+
 TaskHandle_t power_monitoring_task_handle;
 
 
@@ -88,15 +98,21 @@ double RMSCurrent;
 int RMSPower;
 int LineVolts = 230;
 
+eve_history_power_t power_log[EVE_HISTORY_MAX_LOG_ENTRIES];
+
+
 void power_monitoring_task(void *_args) {
 
     static int Current = 0;
     static int MaxCurrent = 0;
     static int MinCurrent = 1023;
     static int PeakCurrent = 0;
+    static int loop_count = EVE_HISTORY_LOG_LOOPS - 1; /*this wil ensure a log entry first tie round the loop */
+    static int total_power_consumed=0;
     
     printf ("%s:\n", __func__);
 
+    
     while (1)
     {
 
@@ -122,10 +138,10 @@ void power_monitoring_task(void *_args) {
         } // End of samples loop
         
         PeakCurrent = MaxCurrent - MinCurrent;
-	printf (" PeakCurrent: %d", PeakCurrent );
+        printf ("%s: PeakCurrent: %d\n", __func__, PeakCurrent );
         
-	RMSCurrent = (PeakCurrent * 0.3535) / Calib;
-	RMSPower = LineVolts * RMSCurrent;
+        RMSCurrent = (PeakCurrent * 0.3535) / Calib;
+        RMSPower = LineVolts * RMSCurrent;
         amps.value = HOMEKIT_FLOAT (RMSCurrent); //Calculates RMS current based on maximum value and scales according to calibration
         watts.value = HOMEKIT_UINT16 (RMSPower );  //Calculates RMS Power Assuming Voltage 240VAC, change to 110VAC accordingly
         volts.value.int_value = LineVolts;
@@ -141,6 +157,27 @@ void power_monitoring_task(void *_args) {
         homekit_characteristic_notify(&volts, volts.value);
         homekit_characteristic_notify(&amps, amps.value);
         homekit_characteristic_notify(&watts, watts.value);
+        
+        
+        loop_count++;
+        total_power_consumed += RMSPower;
+        
+        if ( loop_count == EVE_HISTORY_LOG_LOOPS) {
+            /* we store eve history log ever 10 minutes */
+            
+
+            if(eve_history_log_header.entry_counter = EVE_HISTORY_MAX_LOG_ENTRIES) {
+                /* if we have run out of space, for new log entries start again */
+                eve_history_log_header.entry_counter = 0;
+            }
+            
+            power_log[eve_history_log_header.entry_counter].power_times_10 = (total_power_consumed/EVE_HISTORY_LOG_LOOPS) *10;
+            eve_history_log_header.entry_counter ++;
+            printf ("%s: logging power, %d, log entries, %d\n", __func__, power_log[eve_history_log_header.entry_counter-1].power_times_10, eve_history_log_header.entry_counter);
+            
+            total_power_consumed = 0;
+            loop_count = 0;
+        }
         
         vTaskDelay(POWER_MONITOR_POLL_PERIOD / portTICK_PERIOD_MS);
     }
@@ -180,6 +217,16 @@ homekit_accessory_t *accessories[] = {
             &wifi_check_interval,
             NULL
         }),
+        
+        HOMEKIT_SERVICE(EVE_HISTORY, .primary=true, .characteristics=(homekit_characteristic_t*[]){
+            HOMEKIT_CHARACTERISTIC(NAME, "EVE HISTORY"),
+            &EVE_history_status_S2R1_116,
+            &EVE_history_entries_S2R2_117,
+            &EVE_history_request_S2W1_11C,
+            &EVE_set_time_S2W2_121,
+            NULL
+        }),
+
         NULL
     }),
     NULL
@@ -191,6 +238,12 @@ homekit_server_config_t config = {
     .setupId = "1234",
     .on_event = on_homekit_event
 };
+
+
+void eve_history_send_log(uint32_t starting_from_address){
+    
+    
+}
 
 
 void recover_from_reset (int reason){
@@ -216,6 +269,22 @@ void accessory_init (void ){
 
     load_characteristic_from_flash(&wifi_check_interval);
 
+    /* initialise the eve history stuff*/
+    time_t now;
+    
+    time (&now);
+    
+    eve_history_status.actual_time = (uint32_t)now;
+    eve_history_status.negative_offset = 0 ;
+    eve_history_status.reference_time = 0;
+    eve_history_status.signature_size = 4;
+    eve_history_status.signature = 0x0102020207020f03;
+    eve_history_status.last_memory_position = 0;
+    eve_history_status.unknown = 0;
+    eve_history_status.fixed = 0x01ff;
+    
+    printf ("Size of history header %d, size of history status %d", sizeof ( eve_history_log_header) , sizeof (eve_history_status));
+    
     xTaskCreate(power_monitoring_task, "Power Monitoring Task", 512, NULL, tskIDLE_PRIORITY+1, &power_monitoring_task_handle);
 
 }
